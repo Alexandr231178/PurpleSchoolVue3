2bca50f9c0d2ac465ee86ac56b198379b6bed74d
Alrxandr <alexandr9652764@gmail.com>
2025-09-04 20:31:55 +0500

    three

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+  import Button from "./components/Button.vue"
   const date = new Date();
 </script>
 
 <template>
   <h1>{{ date.toLocaleDateString() }}</h1>
+  <Button>Сохранить</Button>
 </template>
 
 <style scoped>
+
 </style>
